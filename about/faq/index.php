0b0caa57b225805515467f6ac10d800ebceae430
Daniil Dybka <creagoooff@gmail.com>
2023-10-08 01:07:24 +0300

    Edit main apge

--- about/faq/index.php
+++ about/faq/index.php
@@ -10,40 +10,72 @@
     <main>
         <h1>Ответы на вопросы</h1>
         <hr>
-        <div class="accordion" id="accordionExample">
+        <div class="accordion" id="accordion">
             <div class="accordion-item">
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
-                        Регистрация
+                        Открытая регистрация
                     </button>
                 </h2>
-                <div id="collapse1" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div id="collapse1" class="accordion-collapse collapse" data-bs-parent="#accordion">
                     <div class="accordion-body">
-                        Пояснение за регистрацию.
+                        Регистрация в соцсети «Аквариум» бесплатная и без ограничений. Для регистрации вам следует
+                        использовать почту, ввести никнейм и пароль к вашему аккаунту.
                     </div>
                 </div>
             </div>
             <div class="accordion-item">
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                        Accordion Item #2
+                        Активация профиля
                     </button>
                 </h2>
-                <div id="collapse2" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div id="collapse2" class="accordion-collapse collapse" data-bs-parent="#accordion">
                     <div class="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        Для активации профиля вам следует открыть письмо на почте, и пройти по указанной ссылке.
+                        Ссылка доступна в течении нескольких часов, по истечению времени вы сможете повторно запросить
+                        активацию профиля.
                     </div>
                 </div>
             </div>
             <div class="accordion-item">
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                        Accordion Item #3
+                        Доступ к аккаунту
                     </button>
                 </h2>
-                <div id="collapse3" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div id="collapse3" class="accordion-collapse collapse" data-bs-parent="#accordion">
                     <div class="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        После регистрации и активации профиля у вас появится возможность входить в ваш аккаунт без
+                        ограничений на любом устройстве.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                        Доступность платформы
+                    </button>
+                </h2>
+                <div id="collapse4" class="accordion-collapse collapse" data-bs-parent="#accordion">
+                    <div class="accordion-body">
+                        Платформа социальной сети «Аквариум» оснащена доступностью для лиц с
+                        ограниченными возможностями здоровья. Настроено использование платформы
+                        с клавиатуры, используются правильные теги, у изображений настроен
+                        alt атрибут.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+                        API платформы
+                    </button>
+                </h2>
+                <div id="collapse5" class="accordion-collapse collapse" data-bs-parent="#accordion">
+                    <div class="accordion-body">
+                        Для использования на сторонних сервисах у вас есть возможность использовать
+                        API для интеграции входа с помощью соцсети «Аквариум».
                     </div>
                 </div>
             </div>
