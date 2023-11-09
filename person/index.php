2a074649fe1e62020baa4a344652c1438a16d246
Daniil Dybka <creagoooff@gmail.com>
2023-11-09 22:59:08 +0300

    Edit pages

--- person/index.php
+++ person/index.php
@@ -26,17 +26,38 @@
                         </p>
                     </div>
                     <div class="person-profile-content-buttons">
-                        <button class="btn btn-primary">
+                        <button onClick="ButtonLeftBar(4)" class="btn btn-primary me-2">
                             Редактировать профиль
                         </button>
-                        <button class="btn btn-primary">
+                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#desc">
                             Ещё
+                            <svg height="22" width="22" fill="white">
+                                <use xlink:href="/app/img/icons/bootstrap.svg#chevron-down"></use>
+                            </svg>
                         </button>
                     </div>
                 </div>
             </div>
         </div>
     </main>
+
+    <div class="modal fade" id="desc" tabindex="-1" aria-labelledby="descLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <p class="modal-title fs-5 h1 mb-0" id="descLabel">Подробная информация</p>
+                    <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
+                        <svg height="22" width="22" class="svg-normal">
+                            <use xlink:href="/app/img/icons/bootstrap.svg#x-lg"></use>
+                        </svg>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php include_once "../app/php/footer.php"; ?>
 </body>
