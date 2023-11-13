3df1881c364fd334f7fa8b2fab313ce12f625002
Daniil Dybka <creagoooff@gmail.com>
2023-11-13 17:39:31 +0300

    Add more pages

--- app/js/button.js
+++ app/js/button.js
@@ -1,13 +1,15 @@
 function ButtonLeftBar(i) {
-	if (i == 0) {
-		window.open("/feed/", "_self");
-	} else if (i == 1) {
-		window.open("/person/", "_self");
-	} else if (i == 2) {
-		window.open("/notifications/", "_self");
-	} else if (i == 3) {
-		window.open("/search/", "_self");
-	} else if (i == 4) {
-		window.open("/settings/", "_self");
-	}
+	if (i == 0) window.open("/feed/", "_self");
+
+	if (i == 1) window.open("/search/", "_self");
+
+	if (i == 2) window.open("/person/", "_self");
+
+	if (i == 3) window.open("/notifications/", "_self");
+
+	if (i == 4) window.open("/mail/", "_self");
+
+	if (i == 5) window.open("/achivments/", "_self");
+
+	if (i == 6) window.open("/settings/", "_self");
 }
--- app/php/person/left-bar.php
+++ app/php/person/left-bar.php
@@ -11,13 +11,21 @@
         <li>
             <button onClick="ButtonLeftBar(1)" class="w-100">
                 <svg height="28" width="28" class="svg-normal me-3">
+                    <use xlink:href="/app/img/icons/bootstrap.svg#search"></use>
+                </svg>
+                Поиск
+            </button>
+        </li>
+        <li>
+            <button onClick="ButtonLeftBar(2)" class="w-100">
+                <svg height="28" width="28" class="svg-normal me-3">
                     <use xlink:href="/app/img/icons/bootstrap.svg#person"></use>
                 </svg>
                 Профиль
             </button>
         </li>
         <li>
-            <button onClick="ButtonLeftBar(2)" class="w-100">
+            <button onClick="ButtonLeftBar(3)" class="w-100">
                 <svg height="28" width="28" class="svg-normal me-3">
                     <use xlink:href="/app/img/icons/bootstrap.svg#bell"></use>
                 </svg>
@@ -25,15 +33,23 @@
             </button>
         </li>
         <li>
-            <button onClick="ButtonLeftBar(3)" class="w-100">
+            <button onClick="ButtonLeftBar(4)" class="w-100">
                 <svg height="28" width="28" class="svg-normal me-3">
-                    <use xlink:href="/app/img/icons/bootstrap.svg#search"></use>
+                    <use xlink:href="/app/img/icons/bootstrap.svg#envelope"></use>
                 </svg>
-                Поиск
+                Почта
             </button>
         </li>
         <li>
-            <button onClick="ButtonLeftBar(4)" class="w-100">
+            <button onClick="ButtonLeftBar(5)" class="w-100">
+                <svg height="28" width="28" class="svg-normal me-3">
+                    <use xlink:href="/app/img/icons/bootstrap.svg#trophy"></use>
+                </svg>
+                Достижения
+            </button>
+        </li>
+        <li>
+            <button onClick="ButtonLeftBar(6)" class="w-100">
                 <svg height="28" width="28" class="svg-normal me-3">
                     <use xlink:href="/app/img/icons/bootstrap.svg#gear-wide-connected"></use>
                 </svg>
