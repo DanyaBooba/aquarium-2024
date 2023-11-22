0a3a83590d17bbcf3034b65c1c38ceb7b860345b
Daniil Dybka <creagoooff@gmail.com>
2023-11-22 14:00:26 +0300

    Delete lib vendor

--- api/php/login-google.php
+++ api/php/login-google.php
@@ -1,22 +1,43 @@
 <?php
 
 require "../token.php";
-require "../../vendor/autoload.php";
 
-// $parameters = [
-//     'client_id'     => TokenGoogle()[""],
-//     'client_secret' => TokenGoogle()[""],
-//     'redirect_uri'  => GOOGLE_REDIRECT_URI,
-//     'grant_type'    => 'authorization_code',
-//     'code'          => $_GET['code'],
-// ];
+if (!empty($_GET['code'])) {
 
-// $client = new \GuzzleHttp\Client();
+    $params = [
+        'client_id'     => TokenGoogle()["client_id"],
+        'client_secret' => TokenGoogle()["client_secret"],
+        'redirect_uri'  => TokenGoogle()["redirect_uri"],
+        'grant_type'    => 'authorization_code',
+        'code'          => $_GET['code']
+    ];
 
-// $response = $client->post(GOOGLE_TOKEN_URI, ['form_params' => $parameters]);
+    $ch = curl_init('https://accounts.google.com/o/oauth2/token');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $data = curl_exec($ch);
+    curl_close($ch);
 
-// var_dump($response);
+    $data = json_decode($data, true);
+    if (!empty($data['access_token'])) {
 
-// $data = json_decode($response->getBody()->getContents(), true);
+        $params = [
+            'access_token' => $data['access_token'],
+            'id_token'     => $data['id_token'],
+            'token_type'   => 'Bearer',
+            'expires_in'   => 3599
+        ];
 
-// var_dump($data);
+        $info = file_get_contents('https://www.googleapis.com/oauth2/v1/userinfo?' . urldecode(http_build_query($params)));
+        $info = json_decode($info, true);
+
+        ##
+        ## Reg
+        ##
+
+        var_dump($info);
+    }
+}
